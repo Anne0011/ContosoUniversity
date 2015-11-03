a55dfc2e3db7360c23cc6ca084f2aa19fcdc1383
Anne <Anne.grose68@gmail.com>
2015-11-03 12:35:28 -0600

    adding database section to project

---
+++ ContosoUniversity/DAL/SchoolInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolInitializer : System.Data.Enity.DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+
+
+    }
+
+}
